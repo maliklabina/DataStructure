8fef3a036c0da15e7403e3b983d8d62a34fa1f1f
Malik Labina <156318986+Labina-M@users.noreply.github.com>
2024-10-04 16:40:56 +0530

    Update and rename SelectionSort.cpp to ShellSort.cpp
    updated

---
+++ cpp/Array/Sorting/ShellSort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+ 
+void shellSort(int arr[], int size) {
+    for (int gap = size / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < size; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
+    }
+}
+ 
+int main() {
+    int arr[] = {12, 34, 54, 2, 3};
+    int size = sizeof(arr) / sizeof(arr[0]);
+ 
+    shellSort(arr, size);
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
